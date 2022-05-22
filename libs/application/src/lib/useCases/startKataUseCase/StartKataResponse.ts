390563d9f7a7547ab3c091768fe093a934a0b46b
Donny Roufs <iamchets@yahoo.com>
2022-05-22 22:55:53 +0200

    add more props to response

--- libs/application/src/lib/useCases/startKataUseCase/StartKataResponse.ts
+++ libs/application/src/lib/useCases/startKataUseCase/StartKataResponse.ts
@@ -2,13 +2,17 @@ import { Kata } from '@codebarker/domain';
 import { NullOr } from '@codebarker/shared';
 
 export class StartKataResponse {
+  public readonly id: string;
+  public readonly content: string;
   public readonly completedAt: NullOr<Date>;
 
-  private constructor(completedAt: NullOr<Date>) {
+  private constructor(completedAt: NullOr<Date>, id: string, content: string) {
     this.completedAt = completedAt;
+    this.id = id;
+    this.content = content;
   }
 
   public static from(kata: Kata) {
-    return new StartKataResponse(kata.completedAt);
+    return new StartKataResponse(kata.completedAt, kata.id, kata.content);
   }
 }
