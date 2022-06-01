80cd186d2befd2ed1d3f08febfab57a1e07d834e
Donny Roufs <iamchets@yahoo.com>
2022-06-01 19:46:44 +0200

    export button variant as type

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -3,4 +3,5 @@ export { theme } from './lib/Theme';
 export { IconButton } from './lib/buttons/IconButton';
 export { Header } from './lib/header/Header';
 export { Dropdown } from './lib/dropdown/Dropdown';
-export { Button, ButtonVariant } from './lib/buttons/Button';
+export { Button } from './lib/buttons/Button';
+export type { ButtonVariant } from './lib/buttons/Button';
