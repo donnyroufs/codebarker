e3d9fb2599a1a9250c38881b89c5b07f1e5b8710
Donny Roufs <iamchets@yahoo.com>
2022-06-01 22:03:38 +0200

    render learn page in ssr

--- apps/web/pages/learn.tsx
+++ apps/web/pages/learn.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo, useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import { GetServerSideProps } from 'next';
 import {
   Flex,
   Box,
@@ -34,18 +34,12 @@ import { startKata } from './api/startKata';
 import { submitKata } from './api/submitKata';
 import { Layout } from '../components/Layout';
 import { CamelCaseUtil } from '../utils/CamelCaseUtil';
-import { useAuth, useLocalStorage } from '../hooks';
 import { ButtonLink } from '../components';
+import { AuthenticatedGuard } from '../guards/AuthenticatedGuard';
 
-// TODO: Add E2E tests for excludeCompletedKatas
-// TODO: Fix overflow caused by highlighter when too many lines
-export const LearnPage = (): JSX.Element => {
+export const LearnPage = ({ user }: any): JSX.Element => {
   const client = useQueryClient();
-  const { user } = useAuth();
-  const [excludeFilter, setExcludeFilter] = useLocalStorage<boolean>(
-    'codebarker-exclude-finished',
-    true
-  );
+  const [excludeFilter, setExcludeFilter] = useState<boolean>(true);
   const [previousKataId, setPreviousKataId] = useState<undefined | string>(
     undefined
   );
@@ -63,6 +57,7 @@ export const LearnPage = (): JSX.Element => {
 
   useEffect(() => {
     return () => {
+      // Might become an issue later on since I'm not invalidating the exact query
       client.clear();
     };
   }, [client]);
@@ -104,145 +99,144 @@ export const LearnPage = (): JSX.Element => {
   const noAvailableKatas = !data || (data && isError);
 
   return (
-    <Layout>
-      <Container maxW="container.lg" w="100%">
-        <Accordion
-          allowToggle
-          bgColor="brand.panel"
-          color="brand.text"
-          borderRadius={12}
-          p={2}
-          mb={6}
-          w="100%"
-        >
-          <AccordionItem border="none">
-            <Box>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  <Heading as="h1" fontSize="2xl" fontWeight="bold">
-                    Your Filters
-                  </Heading>
-                  <Text>Configure the way smells are generated for you</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
+    <Container maxW="container.lg" w="100%">
+      <Accordion
+        allowToggle
+        bgColor="brand.panel"
+        color="brand.text"
+        borderRadius={12}
+        p={2}
+        mb={6}
+        w="100%"
+      >
+        <AccordionItem border="none">
+          <Box>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                <Heading as="h1" fontSize="2xl" fontWeight="bold">
+                  Your Filters
+                </Heading>
+                <Text>Configure the way smells are generated for you</Text>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </Box>
+          <AccordionPanel pb={4}>
+            <Divider my={2} borderColor="brand.text" height="2px" />
+            <Box mt={4}>
+              <Checkbox
+                isChecked={excludeFilter}
+                onChange={(e): void => onFilterChange(e.target.checked)}
+              >
+                Exclude completed
+              </Checkbox>
             </Box>
-            <AccordionPanel pb={4}>
-              <Divider my={2} borderColor="brand.text" height="2px" />
-              <Box mt={4}>
-                <Checkbox
-                  isChecked={excludeFilter}
-                  onChange={(e): void => onFilterChange(e.target.checked)}
-                >
-                  Exclude completed
-                </Checkbox>
-              </Box>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
 
-        <Skeleton
-          startColor="brand.panel"
-          bgColor="brand.600"
-          endColor="brand.600"
-          fadeDuration={1}
-          p={8}
-          borderRadius={12}
-          w="full"
-          isLoaded={[!isLoading, !isFetching].every((v) => v)}
-        >
-          {noAvailableKatas && (
-            <Alert
-              status="info"
-              variant="solid"
-              bgColor="brand.600"
-              color="brand.text"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              textAlign="center"
-              height="200px"
-            >
-              <AlertIcon boxSize="40px" mr={0} />
-              <AlertTitle mt={4} mb={1} fontSize="lg">
-                You have completed all smells
-              </AlertTitle>
-              <AlertDescription maxWidth="sm">
-                <Flex
-                  justifyContent="center"
-                  alignItems="center"
-                  flexDir="column"
-                >
-                  <Text mb={4}>Keep practicing the ones you completed?</Text>
-                  <HStack spacing={2} justifyContent="center" mt={2}>
-                    <Button
-                      onClick={(): void => onFilterChange(false)}
-                      variant="primary"
-                    >
-                      Yes
-                    </Button>
-                    <ButtonLink href="/" variant="outline">
-                      Back to home
-                    </ButtonLink>
-                  </HStack>
-                </Flex>
-              </AlertDescription>
-            </Alert>
-          )}
-          {!noAvailableKatas && (
-            <>
-              <Box pos="relative">
-                {mutate.isLoading && (
-                  <Spinner
-                    pos="absolute"
-                    top={-2}
-                    right={4}
-                    color="brand.accent"
-                    thickness="3px"
-                  />
-                )}
-
-                <SyntaxHighlighter
-                  language="typescript"
-                  style={atomDark}
-                  customStyle={{
-                    background: '#1C1A31',
-                  }}
-                  showLineNumbers={true}
-                >
-                  {code}
-                </SyntaxHighlighter>
-              </Box>
-              <Divider my={8} borderColor="brand.border" height="2px" />
-              <SimpleGrid minChildWidth="200px" py={2} gap={4}>
-                {data.options.map((opt) => (
+      <Skeleton
+        startColor="brand.panel"
+        bgColor="brand.600"
+        endColor="brand.600"
+        fadeDuration={1}
+        p={8}
+        borderRadius={12}
+        w="full"
+        isLoaded={[!isLoading, !isFetching].every((v) => v)}
+      >
+        {noAvailableKatas && (
+          <Alert
+            status="info"
+            variant="solid"
+            bgColor="brand.600"
+            color="brand.text"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            minH="200px"
+          >
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize="lg">
+              You have completed all smells
+            </AlertTitle>
+            <AlertDescription maxWidth="sm">
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                flexDir="column"
+              >
+                <Text mb={4}>Keep practicing the ones you completed?</Text>
+                <HStack spacing={2} justifyContent="center" mt={2}>
                   <Button
-                    variant="secondary"
-                    bgColor={
-                      !mutate.data?.isCorrect && lastClicked === opt
-                        ? 'red.500'
-                        : 'brand.400'
-                    }
-                    disabled={mutate.isLoading}
-                    key={opt}
-                    onClick={(): Promise<void> => handleSelection(opt)}
+                    onClick={(): void => onFilterChange(false)}
+                    variant="primary"
                   >
-                    {CamelCaseUtil.toReadableString(Smell[opt])}
+                    Yes
                   </Button>
-                ))}
-              </SimpleGrid>
-            </>
-          )}
-        </Skeleton>
-      </Container>
-    </Layout>
+                  <ButtonLink href="/" variant="outline">
+                    Back to home
+                  </ButtonLink>
+                </HStack>
+              </Flex>
+            </AlertDescription>
+          </Alert>
+        )}
+        {!noAvailableKatas && (
+          <>
+            <Box pos="relative">
+              {mutate.isLoading && (
+                <Spinner
+                  pos="absolute"
+                  top={-2}
+                  right={4}
+                  color="brand.accent"
+                  thickness="3px"
+                />
+              )}
+
+              <SyntaxHighlighter
+                language="typescript"
+                style={atomDark}
+                customStyle={{
+                  background: '#1C1A31',
+                }}
+                showLineNumbers={true}
+              >
+                {code}
+              </SyntaxHighlighter>
+            </Box>
+            <Divider my={8} borderColor="brand.border" height="2px" />
+            <SimpleGrid minChildWidth="200px" py={2} gap={4}>
+              {data.options.map((opt) => (
+                <Button
+                  variant="secondary"
+                  bgColor={
+                    !mutate.data?.isCorrect && lastClicked === opt
+                      ? 'red.500'
+                      : 'brand.400'
+                  }
+                  disabled={mutate.isLoading}
+                  key={opt}
+                  onClick={(): Promise<void> => handleSelection(opt)}
+                >
+                  {CamelCaseUtil.toReadableString(Smell[opt])}
+                </Button>
+              ))}
+            </SimpleGrid>
+          </>
+        )}
+      </Skeleton>
+    </Container>
   );
 };
 
-// LearnPage.getLayout = (page: JSX.Element): JSX.Element => (
-//   <Layout>{page}</Layout>
-// );
+LearnPage.getLayout = (page: JSX.Element): JSX.Element => (
+  <Layout>{page}</Layout>
+);
 
-export default dynamic(() => Promise.resolve(LearnPage), {
-  ssr: false,
-});
+export const getServerSideProps: GetServerSideProps =
+  AuthenticatedGuard.execute;
+
+export default LearnPage;
