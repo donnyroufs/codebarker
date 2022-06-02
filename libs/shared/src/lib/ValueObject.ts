947366ed89a98b70fe6f8432cb8b4f017117c31a
Donny Roufs <iamchets@yahoo.com>
2022-06-02 19:49:21 +0200

    add todos

--- libs/shared/src/lib/ValueObject.ts
+++ libs/shared/src/lib/ValueObject.ts
@@ -1,5 +1,7 @@
 import { shallowEqual } from 'shallow-equal-object';
 
+// TODO: Check if it works with arrays as values
+// TODO: Add toJSON method
 export abstract class ValueObject {
   public equals(valueObject: ValueObject): boolean {
     return shallowEqual(Object.values(this), Object.values(valueObject));
