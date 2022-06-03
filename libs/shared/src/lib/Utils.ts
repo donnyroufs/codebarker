b69a3de9ecd34db63cb8f23f3cac8986cdb8b28d
Donny Roufs <iamchets@yahoo.com>
2022-06-03 17:06:36 +0200

    add cast utility

--- libs/shared/src/lib/Utils.ts
+++ libs/shared/src/lib/Utils.ts
@@ -3,3 +3,7 @@ import { NullOr } from './types';
 export function isNull<T>(value: NullOr<T>): value is null {
   return value === null;
 }
+
+export function cast<T>(target: any): T {
+  return target as T;
+}
