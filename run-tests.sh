09c42b68817e4e5cfabe0aeb208e6bef62bcdd99
Donny Roufs <iamchets@yahoo.com>
2022-08-22 16:52:14 +0200

    ci: merge test with ci workflow

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,5 @@
 docker-compose -f docker-compose.test.yml up -d
 sleep 5
-yarn test:ci
+npx nx affected --target=test --parallel --max-parallel=2
+npx nx affected --target=test-integration 
+docker-compose down
